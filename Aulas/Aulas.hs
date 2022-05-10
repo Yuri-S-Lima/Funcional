7613b7effa5d499a418b2442a9c832cafdbac9ce
Yuri S. Lima <61335084+Yuri-S-Lima@users.noreply.github.com>
2022-05-10 19:10:15 -0300

    Comentários
    Adição de comentários que especificam o que cada função faz.

--- Aulas/Aulas.hs
+++ Aulas/Aulas.hs
@@ -1,21 +1,22 @@
-potencia :: Int -> Int -> Int
+potencia :: Int -> Int -> Int {-Função Potência-}
 potencia n m
     |m == 0 = 1
+    |n == 1 = n
     |m > 0 = n * potencia n (m - 1)
 
-divide :: Int -> Int -> Int
+divide :: Int -> Int -> Int {-Função que retorna a quantidade divisões feitas-}
 divide x y
     |x == y = 1
     |x < y = 0
     |otherwise = 1 + divide (x - y) y
 
-modulo :: Int -> Int -> Int
+modulo :: Int -> Int -> Int {-Retorna o resto da divisão-}
 modulo x y
     |x == y || y == 1 = 0
     |x < y = x
     |x > y = modulo (x - y) y
 
-primo :: Int -> Bool
+primo :: Int -> Bool {-Função Primo, verifica se determinado número é ou não primo-}
 auxPrimo :: Int -> Int -> Bool
 primo n
     |modulo n 2 == 0 && n /= 2 = False
@@ -27,7 +28,7 @@ auxPrimo x y
     |modulo x y == 0 = False
     |otherwise = auxPrimo x (y - 1)
 
-perfeito :: Int -> Bool
+perfeito :: Int -> Bool {-Função que verifica se determinado número é ou não perfeito-}
 auxPerfeito :: Int -> Int -> Int -> Bool
 perfeito n
     |n < 6 = False
@@ -39,17 +40,17 @@ auxPerfeito x y z
     |modulo x z == 0 = auxPerfeito x (y + z) (z + 1)
     |otherwise = auxPerfeito x y (z + 1)
 
-algarismos :: Double -> Int
+algarismos :: Double -> Int {-Função que conta a quantidade de algarismos que formam determinado número-}
 algarismos n
     |n < 10 = 1
     |otherwise = 1 + algarismos (n/10)
 
-fatorial :: Int -> Int
+fatorial :: Int -> Int {-Função fatorial-}
 fatorial n
     |n == 0 || n == 1 = 1
     |otherwise = n * fatorial (n - 1)
 
-valorFat :: Int -> Int
+valorFat :: Int -> Int {-Função que retorna o valor do resultado de um fatorial-}
 auxFat :: Int -> Int -> Int -> Int
 valorFat m
     |m == 1 = 1
@@ -60,18 +61,16 @@ auxFat m n x
     |x == m = n
     |otherwise = auxFat m (n + 1) (x * (n + 1))
 
-somaFat :: Int -> Int
+somaFat :: Int -> Int {-Função que soma os fatoriais no intervalo entre 0 e n-}
 somaFat n
     |n == 0 = 1
     |otherwise = fatorial(n) + somaFat (n - 1)
 
-fib :: Int -> Int
-aFib :: Int -> Int -> Int -> Int
-fib n
-    |n == 0 = 0
-    |n == 1 = 1
-    |otherwise = aFib n 1 1
+nPrimo :: Int -> Int {-Função que retorna o n-ésimo primo-}
+cont :: Int -> Int -> Int
+nPrimo n = cont n 2
 
-aFib n m p
-    |p == n = p
-    |otherwise = aFib n p (m + p)
+cont n p
+    |n == 0 = p - 1
+    |primo p = cont (n - 1) (p + 1)
+    |otherwise = cont n (p + 1)
