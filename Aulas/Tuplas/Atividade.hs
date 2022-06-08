db24ce3fdfee2f85e0e65aeddc4f91437937f765
Yuri S. Lima <61335084+Yuri-S-Lima@users.noreply.github.com>
2022-06-08 14:34:14 -0300

    Tuplas
    Algumas questões sobre tuplas

--- Aulas/Tuplas/Atividade.hs
+++ Aulas/Tuplas/Atividade.hs
@@ -12,15 +12,9 @@ segundos (h, m, s)
     |not (verifica (h, m, s)) = 0
     |otherwise = (h * (60 * 60)) + (m * 60) + s
 
--- aux :: Int -> Int -> Int -> Int -> Hora
 converte :: Int -> Hora
 converte s
     |s <= 0 = (0,0,0)
-    |otherwise = ((div s 3600), (div (s - 3600) (60)), (mod (s - 3600) 60))
-
--- aux t h m ss 
---     |t == 0 = (h, m, ss)
---     |otherwise = aux 0 (div t 3600) (div (t - 3600) 60) (mod (t - 3600) 60)
-
--- -- 1h - 3600s
--- -- xh - 4500s
+    |otherwise = (hora, min, seg)
+    where
+        hora = div s 3600; min = (div (s - (3600 * hora)) (60)); seg = (mod (s - 3600) 60);
