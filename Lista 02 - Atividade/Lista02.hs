ac18b7ff4af19a02247ec4e4ca227f399c4fedb8
Yuri S. Lima <61335084+Yuri-S-Lima@users.noreply.github.com>
2022-06-09 11:58:30 -0300

    Adição
    Adicionada a resolução da questão dois da Lista02

--- Lista 02 - Atividade/Lista02.hs
+++ Lista 02 - Atividade/Lista02.hs
@@ -5,19 +5,19 @@ impares = [x | x <- [1..100], odd x]
 
 -- b. Pares entre 10 e 100
 pares :: [Int]
-pares = [x | x <- [1..100], x > 10, even x]
+pares = [x | x <- [10..100], x > 10, even x]
 
 -- c. Ímpares entre 1 e N
 imparesN :: Int -> [Int]
-imparesN n = [x | x <- [1..n-1], odd x]
+imparesN n = [x | x <- [1..n], odd x]
 
 -- d. Números entre 1 e N que são múltiplos de 3 e 5 ao mesmo tempo.
 multiplos :: Int -> [Int]
-multiplos n = [x | x <- [1..n-1], mod x 3 == 0, mod x 5 == 0]
+multiplos n = [x | x <- [1..n], mod x 3 == 0, mod x 5 == 0]
 
 -- e. Tuplas entre 1 e N, contendo o número e seu respectivo quadrado.
 duplas :: Int -> [(Int, Int)]
-duplas n = [(x, x*2) | x <- [1..n]]
+duplas n = [(x, x * x) | x <- [1..n]]
 
 -- f. Tuplas com os índices de uma matriz 3x4.
 matriz :: [(Int, Int)]
@@ -27,8 +27,14 @@ matriz = [(x, y) | x <- [1..3], y <- [1..4]]
 matrizNM :: Int -> Int -> [(Int, Int)]
 matrizNM n m = [(x, y) | x <- [1..n], y <- [1..m]]
 
-fib :: Int -> Int {-Função que retorna o fibonacci de n-}
+-- 2. Escreva uma função com a seguinte assinatura listaFibonacci :: Int->[Int] que retorna uma lista
+-- com os n primeiros números da sequência de Fibonacci.
+fib :: Int -> Int {-Função que retorna o fibonacci de n primeiros números-}
 fib n
     |n == 1 = 0
     |n == 2 = 1
     |otherwise = fib(n - 1) + fib(n - 2)
+
+listaFibonacci :: Int -> [Int]
+listaFibonacci 0 = []
+listaFibonacci n = [fib y | y <- [1..n]]
